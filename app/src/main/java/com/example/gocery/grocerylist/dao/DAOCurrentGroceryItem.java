388db422fb847fb97b8fa8e67de107e304b4dafe
Ajwad <ajwad.alias@gmail.com>
2022-01-27 11:04:37 +0800

    updated, save now onto user's specific account

--- app/src/main/java/com/example/gocery/grocerylist/dao/DAOCurrentGroceryItem.java
+++ app/src/main/java/com/example/gocery/grocerylist/dao/DAOCurrentGroceryItem.java
@@ -1,7 +1,11 @@
 package com.example.gocery.grocerylist.dao;
 
+import android.util.Log;
+
 import com.example.gocery.grocerylist.model.GroceryItem;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -11,8 +15,10 @@ import java.util.HashMap;
 public class DAOCurrentGroceryItem {
 
     private DatabaseReference databaseReference;
+    private String currentUser;
 
     public DAOCurrentGroceryItem() {
+        currentUser = "user_"+FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         databaseReference = db.getReference(GroceryItem.class.getSimpleName());
     }
@@ -20,23 +26,23 @@ public class DAOCurrentGroceryItem {
 
     public Task<Void> add(GroceryItem groceryItem)
     {
-        return databaseReference.push().setValue(groceryItem);
+        return databaseReference.child(currentUser).push().setValue(groceryItem);
     }
 
     public Task<Void> update(String key, HashMap<String, Object> hashMap){
-        return databaseReference.child(key).updateChildren(hashMap);
+        return databaseReference.child(currentUser).child(key).updateChildren(hashMap);
     }
 
     public Task<Void> remove(String key) {
-        return databaseReference.child(key).removeValue();
+        return databaseReference.child(currentUser).child(key).removeValue();
     }
 
     public Query get() {
-        return databaseReference;
+        return databaseReference.child(currentUser);
     }
 
     public Query getSingle(String item_key){
-        return databaseReference.child(item_key);
+        return databaseReference.child(currentUser).child(item_key);
     }
 
 
--- app/src/main/java/com/example/gocery/grocerylist/dao/DAOFinalizeGroceryList.java
+++ app/src/main/java/com/example/gocery/grocerylist/dao/DAOFinalizeGroceryList.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.example.gocery.grocerylist.model.GroceryItem;
 import com.example.gocery.grocerylist.model.GroceryTrip;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -15,8 +16,10 @@ import java.util.HashMap;
 
 public class DAOFinalizeGroceryList {
     private DatabaseReference databaseReferenceGroceryItem, databaseReferenceGroceryTrip;
+    private String currentUser;
 
     public DAOFinalizeGroceryList() {
+        currentUser = "user_"+FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         databaseReferenceGroceryItem = db.getReference(GroceryItem.class.getSimpleName());
         databaseReferenceGroceryTrip = db.getReference(GroceryTrip.class.getSimpleName());
@@ -25,7 +28,7 @@ public class DAOFinalizeGroceryList {
 
 
     public Query getTickedItems(){
-        return databaseReferenceGroceryItem.orderByChild("status").equalTo(true);
+        return databaseReferenceGroceryItem.child(currentUser).orderByChild("status").equalTo(true);
     }
 
     public Task<Void> finalizeGrocery(GroceryTrip groceryTrip){
@@ -35,11 +38,11 @@ public class DAOFinalizeGroceryList {
 
         // remove data from current grocery items
         for (GroceryItem groceryItem: groceryTrip.getGroceryItems()) {
-            databaseReferenceGroceryItem.child(groceryItem.getKey()).removeValue();
+            databaseReferenceGroceryItem.child(currentUser).child(groceryItem.getKey()).removeValue();
         }
 
         //save new grocery trip
-        return databaseReferenceGroceryTrip.child(tripKey).setValue(groceryTrip);
+        return databaseReferenceGroceryTrip.child(currentUser).child(tripKey).setValue(groceryTrip);
     }
 
 
