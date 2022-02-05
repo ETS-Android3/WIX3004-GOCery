7897a6cd5ca850f851543c9713e72f778ad8490c
Ajwad <ajwad.alias@gmail.com>
2022-02-05 22:49:32 +0800

    isolated data for FAIDZ's part

--- app/src/main/java/com/example/gocery/card_voucher/dao/DAOVoucher.java
+++ app/src/main/java/com/example/gocery/card_voucher/dao/DAOVoucher.java
@@ -2,6 +2,7 @@ package com.example.gocery.card_voucher.dao;
 
 import com.example.gocery.card_voucher.model.Voucher;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -10,10 +11,12 @@ import java.util.HashMap;
 
 public class DAOVoucher {
     private DatabaseReference dbRef;
+    private String currentUser;
 
     public DAOVoucher(){
+        currentUser = "user_"+ FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        dbRef = db.getReference(Voucher.class.getSimpleName());
+        dbRef = db.getReference(Voucher.class.getSimpleName()).child(currentUser);
     }
 
     public Task<Void> addVoucher(Voucher voucher) {
