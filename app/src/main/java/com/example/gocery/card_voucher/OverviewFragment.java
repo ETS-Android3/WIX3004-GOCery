3d56202f556e1e720bb2ac9b873e708f6957d12e
faidzhazirah <faidzhazirah@gmail.com>
2022-02-06 17:38:37 +0800

    Updated fragment title for overview fragment

--- app/src/main/java/com/example/gocery/card_voucher/OverviewFragment.java
+++ app/src/main/java/com/example/gocery/card_voucher/OverviewFragment.java
@@ -2,6 +2,8 @@ package com.example.gocery.card_voucher;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -91,8 +94,16 @@ public class OverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Cards and Vouchers");
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
